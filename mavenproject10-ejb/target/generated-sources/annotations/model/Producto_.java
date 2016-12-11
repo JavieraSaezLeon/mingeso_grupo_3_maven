e5b68dee634420c617f2adc2d7723cb8022ea8bb
javiera <javiera.saezl@usach.cl>
2016-12-11 02:05:04 -0400

    prueba1

--- mavenproject10-ejb/target/generated-sources/annotations/model/Producto_.java
+++ mavenproject10-ejb/target/generated-sources/annotations/model/Producto_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-11T00:52:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-11T03:03:14")
 @StaticMetamodel(Producto.class)
 public class Producto_ { 
 
