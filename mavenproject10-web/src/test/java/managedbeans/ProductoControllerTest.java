447f6fdc9dbe19a3f5905720920173bf66642723
javiera <javiera.saezl@usach.cl>
2016-12-11 17:09:00 -0300

    prueba5

--- mavenproject10-web/src/test/java/managedbeans/ProductoControllerTest.java
+++ mavenproject10-web/src/test/java/managedbeans/ProductoControllerTest.java
@@ -157,5 +157,6 @@ public class ProductoControllerTest {
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
     }
+    * probando pulling
     */
 }
